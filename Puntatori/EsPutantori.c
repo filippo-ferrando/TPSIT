3915ca09436f77a85b72b857733ab298a5015f60
filippoferrando <ferrandodfilippo@gmail.com>
2020-10-01 12:58:20 +0200

    new exercise

--- Puntatori/EsPutantori.c
+++ Puntatori/EsPutantori.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void main(){
-    int *pi;
+void main()
+{
+    int* pi;
     *pi = 15;
     printf("%p\n", pi);
     pi = pi + 1;
     printf("%p\n", pi);
+    *pi = 20;
 }
